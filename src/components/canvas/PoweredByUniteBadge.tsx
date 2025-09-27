5d47f4d8a5cf72c4b881dfbacb4ae846d0595c2a
wevemedeiros <wguedes@refmais.com.br>
2025-09-27 15:34:51 -0300

    mudança logo

---
+++ src/components/canvas/PoweredByUniteBadge.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export const PoweredByUniteBadge: React.FC = () => {
+  return (
+    <div className="absolute top-4 left-4 z-20 hidden md:block">
+      <Link
+        href="https://myunite.ai"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="border bg-card p-2 flex flex-row rounded-xl items-center"
+      >
+        <Image
+          src="https://storage.googleapis.com/unite_assets/logos/Unite%20Logo%20Negativo.png"
+          alt="Unite logo"
+          width={120}
+          height={36}
+          priority
+        />
+      </Link>
+    </div>
+  );
+};
